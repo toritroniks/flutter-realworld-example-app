9f3614022523d049c7824655e024c1398e7a4443
Ravi Souza <ravi.c.souza@gmail.com>
2021-06-07 14:49:14 +0900

    Smartphone responsiveness

--- lib/responsive.dart
+++ lib/responsive.dart
@@ -16,16 +16,16 @@ class Responsive extends StatelessWidget {
       MediaQuery.of(context).size.width < 850;
 
   static bool isTablet(BuildContext context) =>
-      MediaQuery.of(context).size.width < 1100 &&
+      MediaQuery.of(context).size.width < 1000 &&
       MediaQuery.of(context).size.width >= 850;
 
   static bool isDesktop(BuildContext context) =>
-      MediaQuery.of(context).size.width >= 1100;
+      MediaQuery.of(context).size.width >= 1000;
 
   @override
   Widget build(BuildContext context) {
     final _size = MediaQuery.of(context).size;
-    if (_size.width >= 1100) {
+    if (_size.width >= 1000) {
       return desktop;
     } else if (_size.width >= 850 && tablet != null) {
       return tablet!;
--- lib/widgets/custom_base.dart
+++ lib/widgets/custom_base.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_realworld/responsive.dart';
 import 'package:flutter_realworld/widgets/custom_app_bar.dart';
 import 'package:flutter_realworld/widgets/custom_content_area.dart';
 
@@ -14,6 +15,26 @@ class CustomBase extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return Responsive(
+      desktop: _CustomBaseContent(banner: banner, child: child),
+      mobile: _ScaledContent(
+        child: _CustomBaseContent(banner: banner, child: child),
+      ),
+    );
+  }
+}
+
+class _CustomBaseContent extends StatelessWidget {
+  const _CustomBaseContent({
+    required this.banner,
+    required this.child,
+  });
+
+  final Widget? banner;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
     return SingleChildScrollView(
       child: Column(
         children: [
@@ -22,6 +43,33 @@ class CustomBase extends StatelessWidget {
           CustomContentArea(child: child),
         ],
       ),
+    );
+  }
+}
+
+class _ScaledContent extends StatelessWidget {
+  const _ScaledContent({required this.child});
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        final logicWidth = 1000.0;
+        final logicHeight =
+            logicWidth * (constraints.maxHeight / constraints.maxWidth);
+        return SizedBox.expand(
+          child: FittedBox(
+            fit: BoxFit.contain,
+            child: SizedBox(
+              width: logicWidth,
+              height: logicHeight,
+              child: child,
+            ),
+          ),
+        );
+      },
     );
   }
 }
